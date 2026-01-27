fd5fcbe14c4c8e05caee770d47ea87ade7e80a12
dapraws <darrelprawira26@gmail.com>
2026-01-27 08:36:56 +0700

    feat: tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
