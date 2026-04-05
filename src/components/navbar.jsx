3328ab144ab07218b65108372618e481ba283cde
Dzamir Akmal <dzamir169@gmail.com>
2026-04-05 20:14:31 +0700

    deleted:    src/components/Lazyimage.jsx 	modified:   src/components/footer.jsx 	modified:   src/components/navbar.jsx 	modified:   src/pages/Home/Home.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -14,7 +14,6 @@ const Navbar = () => {
     { path: "/", label: "Home" },
     { path: "/services", label: "Services" },
     { path: "/booking", label: "Booking" },
-    { path: "/summary", label: "Summary" },
   ];
 
   const calculateHeight = () => {
